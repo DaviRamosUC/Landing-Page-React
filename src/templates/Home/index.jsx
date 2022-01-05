3bbe10ba0af2f22b3b7787a762703a54f275b23c
Davi Ramos <ifbadavi@gmail.com>
2022-01-04 21:13:50 -0300

    configurando styled-components

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,7 +1,13 @@
-import './App.css';
+import * as Styled from './styles';
 
 function Home() {
-  return <h1>Hello</h1>;
+  return (
+    <div>
+      <Styled.Wrapper>
+        <h1>Hello</h1>
+      </Styled.Wrapper>
+    </div>
+  );
 }
 
 export default Home;
