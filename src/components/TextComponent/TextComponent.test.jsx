9edbf2626648713d56b818e344fa3b4afe403587
Davi Ramos <ifbadavi@gmail.com>
2022-01-07 18:34:25 -0300

    Realizando teste do component

--- src/components/TextComponent/TextComponent.test.jsx
+++ src/components/TextComponent/TextComponent.test.jsx
@@ -1,11 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import { TextComponent } from '.';
 import { renderTheme } from '../../styles/render-theme';
+import { theme } from '../../styles/theme';
 
 describe('<TextComponent/>', () => {
-  it('should render', () => {
+  it('should render TextComponent', () => {
     renderTheme(<TextComponent>children</TextComponent>);
+    const paragraph = screen.getByText('children');
 
-    expect(screen.getByRole('heading')).toBeInTheDocument();
+    expect(paragraph).toBeInTheDocument();
+  });
+
+  it('should match snapshot', () => {
+    const { container } = renderTheme(<TextComponent>children</TextComponent>);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('should render a paragraph with 24px font-size', () => {
+    const { debug } = renderTheme(<TextComponent>children</TextComponent>);
+    const paragraph = screen.getByText('children');
+
+    expect(paragraph).toHaveStyleRule('font-size', theme.font.sizes.medium);
   });
 });
