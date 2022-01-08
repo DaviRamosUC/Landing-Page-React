61635236bf9a324910e90af11f35c75aa4c6a79d
Davi Ramos <ifbadavi@gmail.com>
2022-01-07 21:08:49 -0300

    Criando componente MenuLink com teste

---
+++ src/components/MenuLink/MenuLink.test.jsx
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react';
+import { MenuLink } from '.';
+import { renderTheme } from '../../styles/render-theme';
+
+describe('<MenuLink/>', () => {
+  it('should render a link', () => {
+    renderTheme(<MenuLink link="http://localhost">children</MenuLink>);
+
+    expect(screen.getByRole('link', { name: 'children' })).toHaveAttribute(
+      'target',
+      '_self',
+    );
+  });
+
+  it('should open in a new tab', () => {
+    renderTheme(
+      <MenuLink link="http://localhost" newTab={true}>
+        children
+      </MenuLink>,
+    );
+
+    expect(screen.getByRole('link', { name: 'children' })).toHaveAttribute(
+      'target',
+      '_blank',
+    );
+  });
+
+  it('should open in a new tab', () => {
+    renderTheme(
+      <MenuLink link="http://localhost" newTab={false}>
+        children
+      </MenuLink>,
+    );
+
+    expect(screen.getByRole('link', { name: 'children' })).toMatchSnapshot();
+  });
+});
